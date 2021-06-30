b0af9fb7933a3239f9363295180ae45d1e281982
Alexis Bouchez <bouchezalexis1@gmail.com>
2021-06-30 14:12:08 +0200

    frenchify the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<footer className='footer'>
 			<div className='content has-text-centered'>
-				Bon<strong>Livre</strong> built by{' '}
+				Bon<strong>Livre</strong> développé par{' '}
 				<a href='https://alexisbouchez.com'>Alexis Bouchez</a>.
 			</div>
 		</footer>
